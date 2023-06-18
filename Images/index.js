e32af31940aaddeed4308f2145d220cbcb1b0dd0
Md Jibon Akando <135594576+mdjibon559722@users.noreply.github.com>
2023-06-18 10:15:33 +0600

    Add files via upload

---
+++ Images/index.js
@@ -0,0 +1,7 @@
+var navemenus = document.getElementById("navemenus");
+    function openMenu(){
+      navemenus.style.right = "0";
+    }
+    function hideMenu(){
+      navemenus.style.right = "-300px";
+    }
